253be64441582f34ebc0a37ee8c50c14147d5bd2
EarthDragon10 <lagudadev@gmail.com>
2022-10-24 17:06:15 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ let player_1 = 4;
 let player_2 = 10;
 let randomNum = Math.floor((Math.random() * 100) + 1);
 const getNumPlayer_1 = () => {
-    let inputPlayer_1 = document.querySelector("#player_1");
-    let numPlayer_1 = Number(inputPlayer_1);
+    // let inputPlayer_1: HTMLInputElement = document.querySelector("#player_1");
+    // let numPlayer_1: number = Number(inputPlayer_1.value);
 };
 const comparePlayerToNum = () => {
     if (player_1 === randomNum && player_2 !== randomNum) {
